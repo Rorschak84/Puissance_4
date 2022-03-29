62f94ad98159dd0ebbd3de08e3205a7ed74040b9
Simon Langlais <simon.langlais@ecl20.ec-lyon.fr>
2022-03-29 19:29:36 +0200

    changement mineurs

--- puissance.js
+++ puissance.js
@@ -1,43 +1,39 @@
 class Puissance4 {
   /*
-    Intialise un plateau de jeu de dimensions `rows` × `cols` (par défaut 6×7),
-    et fait l'affichage dans l'élément `element_id` du DOM.
+   Crée un plateau de Jeu de dimension 6*7 (par défaut) 
    */
-  constructor(element_id, rows=6, cols=7) {
+  constructor(elt_id, lignes=6, colonne=7) {
     // Nombre de lignes et de colonnes
-    this.rows = rows;
-    this.cols = cols;
-    //initialisation du plateau
-    // cet tableau à deux dimensions contient l'état du jeu:
+    this.lignes = lignes;
+    this.colonne = colonne;
+    //On initialise le plateau
+    // tableau 2D qui contient un entier
     //   0: case vide
     //   1: pion du joueur 1
     //   2: pion du joueur 2
-    this.board = Array(this.rows);
-    for (let i = 0; i < this.rows; i++) {
-      this.board[i] = Array(this.cols).fill(0);
+    this.plateau = Array(this.lignes);
+    for (let i = 0; i < this.lignes; i++) {
+      this.plateau[i] = Array(this.colonne).fill(0);
     }
     // un entier: 1 ou 2 (le numéro du prochain joueur)
-    this.turn = 1;
+    this.tour = 1;
     // Nombre de coups joués
-    this.moves = 0;
+    this.nb_coup = 0;
     /* un entier indiquant le gagnant:
         null: la partie continue
            0: la partie est nulle
            1: joueur 1 a gagné
            2: joueur 2 a gagné
     */
-    this.winner = null;
+    this.gagnant = null;
 
     // L'élément du DOM où se fait l'affichage
-    this.element = document.querySelector(element_id);
+    this.element = document.querySelector(elt_id);
     // On ajoute le gestionnaire d'événements pour gérer le click
     //
-    // Pour des raisons techniques, il est nécessaire de passer comme gestionnaire
-    // une fonction anonyme faisant appel à `this.handle_click`. Passer directement
-    // `this.handle_click` comme gestionnaire, sans wrapping, rendrait le mot clef
-    // `this` inutilisable dans le gestionnaire. Voir le "binding de this".
+
     this.element.addEventListener('click', (event) => this.handle_click(event));
-    // On fait l'affichage
+    // On fait l'affichage du plateau
     this.render();
   }
   
@@ -46,52 +42,51 @@ class Puissance4 {
   /* Affiche le plateau de jeu dans le DOM */
   render() {
     let table = document.createElement('table');
-    //ATTENTION, la page html est écrite de haut en bas. Les indices 
-    //pour le jeu vont de bas en haut (compteur i de la boucle)
-    for (let i = this.rows - 1; i >= 0; i--) {
+    //Les indices pour le jeu vont de bas en haut 
+    for (let i = this.lignes - 1; i >= 0; i--) {
       let tr = table.appendChild(document.createElement('tr'));
-      for (let j = 0; j < this.cols; j++) {
+      for (let j = 0; j < this.colonne; j++) {
         let td = tr.appendChild(document.createElement('td'));
-        let colour = this.board[i][j];
+        let colour = this.plateau[i][j];
         if (colour)
           td.className = 'player' + colour;
-        td.dataset.column = j;
+        td.dataset.la_colonne = j;
       }
     }
     this.element.innerHTML = '';
     this.element.appendChild(table);
   }
   
-  set(row, column, player) {
-    // On colore la case
-    this.board[row][column] = player;
-    // On compte le coup
-    this.moves++;
+  set(li, la_colonne, player) {
+    // On colore la case en fonction du tour actuel
+    this.plateau[li][la_colonne] = player;
+    // On incrémente le coup
+    this.nb_coup++;
   }
 
   /* Cette fonction ajoute un pion dans une colonne */
-  play(column) {
+  play(la_colonne) {
     // Trouver la première case libre dans la colonne
-    let row;
-    for (let i = 0; i < this.rows; i++) {
-      if (this.board[i][column] == 0) {
-        row = i;
+    let li;
+    for (let i = 0; i < this.lignes; i++) {
+      if (this.plateau[i][la_colonne] == 0) {
+        li = i;
         break;
       }
     }
-    if (row === undefined) {
+    if (li === undefined) {
       return null;
     } else {
       // Effectuer le coup
-      this.set(row, column, this.turn);
+      this.set(li, la_colonne, this.tour);
       // Renvoyer la ligne où on a joué
-      return row;
+      return li;
     }
   }
   
   handle_click(event) {
-    // Vérifier si la partie est encore en cours
-    if (this.winner !== null) {
+    // Vérifier si la partie est encore en cours, sinon on demande au joueur s'il veut rejouer
+    if (this.gagnant !== null) {
       if (window.confirm("Game over!\n\nDo you want to restart?")) {
         this.reset();
         this.render();
@@ -99,40 +94,38 @@ class Puissance4 {
       return;
     }
 
-    let column = event.target.dataset.column;
-    if (column !== undefined) {
-      //attention, les variables dans les datasets sont TOUJOURS 
-      //des chaînes de caractères. Si on veut être sûr de ne pas faire de bêtise,
-      //il vaut mieux la convertir en entier avec parseInt
-      column = parseInt(column);
-      let row = this.play(parseInt(column));
+    let la_colonne = event.target.dataset.la_colonne;
+    if (la_colonne !== undefined) {
+      //on convertit la variable contenue dans le dataset car c'est une chaine de caractère initialement
+      la_colonne = parseInt(la_colonne);
+      let li = this.play(parseInt(la_colonne));
       
-      if (row === null) {
-        window.alert("Column is full!");
+      if (li === null) {
+        window.alert("la colonne est pleine!");
       } else {
         // Vérifier s'il y a un gagnant, ou si la partie est finie
-        if (this.win(row, column, this.turn)) {
-          this.winner = this.turn;
-        } else if (this.moves >= this.rows * this.columns) {
-          this.winner = 0;
+        if (this.win(li, la_colonne, this.tour)) {
+          this.gagnant = this.tour;
+        } else if (this.nb_coup >= this.lignes * this.columns) {
+          this.gagnant = 0;
         }
         // Passer le tour : 3 - 2 = 1, 3 - 1 = 2
-        this.turn = 3 - this.turn;
+        this.tour = 3 - this.tour;
 
         // Mettre à jour l'affichage
         this.render()
         
         //Au cours de l'affichage, pensez eventuellement, à afficher un 
         //message si la partie est finie...
-        switch (this.winner) {
+        switch (this.gagnant) {
           case 0: 
-            window.alert("Null game!!"); 
+            window.alert("Match nul!!"); 
             break;
           case 1:
-            window.alert("Player 1 wins"); 
+            window.alert("Player 1 Gagne"); 
             break;
           case 2:
-            window.alert("Player 2 wins"); 
+            window.alert("Player 2 Gagne"); 
             break;
         }
       }
@@ -140,39 +133,36 @@ class Puissance4 {
   }
 
   /* 
-   Cette fonction vérifie si le coup dans la case `row`, `column` par
-   le joueur `player` est un coup gagnant.
+   Cette fonction vérifie si le coup  est un coup gagnant.
    
-   Renvoie :
-     true  : si la partie est gagnée par le joueur `player`
-     false : si la partie continue
+   Elle renvoie true si gagné par le joueur, false sinon
  */
-  win(row, column, player) {
+  win(li, la_colonne, player) {
     // Horizontal
     let count = 0;
-    for (let j = 0; j < this.cols; j++) {
-      count = (this.board[row][j] == player) ? count+1 : 0;
+    for (let j = 0; j < this.colonne; j++) {
+      count = (this.plateau[li][j] == player) ? count+1 : 0;
       if (count >= 4) return true;
     }
     // Vertical
     count = 0;
-    for (let i = 0; i < this.rows; i++) {
-      count = (this.board[i][column] == player) ? count+1 : 0;
+    for (let i = 0; i < this.lignes; i++) {
+      count = (this.plateau[i][la_colonne] == player) ? count+1 : 0;
       if (count >= 4) return true;
     }
     // Diagonal
     count = 0;
-    let shift = row - column;
-    for (let i = Math.max(shift, 0); i < Math.min(this.rows, this.cols + shift); i++) {
-      count = (this.board[i][i - shift] == player) ? count+1 : 0;
+    let shift = li - la_colonne;
+    for (let i = Math.max(shift, 0); i < Math.min(this.lignes, this.colonne + shift); i++) {
+      count = (this.plateau[i][i - shift] == player) ? count+1 : 0;
       if (count >= 4) return true;
     }
     // Anti-diagonal
     count = 0;
-    shift = row + column;
-    for (let i = Math.max(shift - this.cols + 1, 0); i < Math.min(this.rows, shift + 1); i++) {
+    shift = li + la_colonne;
+    for (let i = Math.max(shift - this.colonne + 1, 0); i < Math.min(this.lignes, shift + 1); i++) {
       console.log(i,shift-i,shift)
-      count = (this.board[i][shift - i] == player) ? count+1 : 0;
+      count = (this.plateau[i][shift - i] == player) ? count+1 : 0;
       if (count >= 4) return true;
     }
     
@@ -181,13 +171,13 @@ class Puissance4 {
 
   // Cette fonction vide le plateau et remet à zéro l'état
   reset() {
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        this.board[i][j] = 0;
+    for (let i = 0; i < this.lignes; i++) {
+      for (let j = 0; j < this.colonne; j++) {
+        this.plateau[i][j] = 0;
       }
     }
-    this.move = 0;
-    this.winner = null;
+    this.nb_coup = 0;
+    this.gagnant = null;
   }
 }
 
